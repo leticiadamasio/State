41a6b57202036d2df79b38d09aa04d2eb2c6158b
leticiadamasio <damasioleticia03@gmail.com>
2022-02-04 18:57:08 -0300

    State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    nome:"Letícia",
+    idade: 18,
+    comida:"Lasanha",
+    musicas: ["Apê 1001-Bin e Ludmilla","Não é por maldade-Ludmilla", "Oi-Lagum"]
+  }
+  render (){
+    return (
+      <div>
+        <h1>Seu nome é {this.state.nome}</h1>
+        <h2>Você está com {this.state.idade} anos</h2>
+        <h3>Sua comida favorita é {this.state.comida}</h3>
+        <h4>Seu top 3 de músicas é:</h4>
+        <ol>
+          <li>{this.state.musicas[0]}</li>
+          <li>{this.state.musicas[1]}</li>
+          <li>{this.state.musicas[2]}</li>
+        </ol>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App 
